6950f26659293fcdba8c927c8ce70edaaa650daa
Janne Leppänen <janne@janneleppanen.com>
2022-05-29 21:55:03 +0300

    feat: Add multiple AI cars

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import Car from "./car";
-import Controls from "./controls";
 import Road from "./road";
 import "./style.css";
+
+const CAR_COUNT = 10;
 
 const canvas = document.querySelector<HTMLCanvasElement>("#canvas")!;
 const score = document.querySelector<HTMLSpanElement>(".score")!;
@@ -10,23 +11,33 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const road = new Road();
-const car = new Car(road);
-car.x = 0;
-car.angle = Math.PI;
+const cars: Car[] = [];
+for (let i = 0; i < CAR_COUNT; i++) {
+  const car = new Car(road);
+  car.x = 0;
+  car.angle = Math.PI;
+  cars.push(car);
+}
+
+let bestCar = cars[0];
 
 function animate(time = 0) {
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
 
-  car.update();
+  cars.forEach((car) => car.update());
 
   ctx.save();
-  ctx.translate(-car.x + canvas.width / 2, -car.y + canvas.height / 2);
+  ctx.translate(-bestCar.x + canvas.width / 2, -bestCar.y + canvas.height / 2);
   road.draw(ctx);
-  car.draw(ctx);
+  cars.forEach((car) => car.draw(ctx, false));
   ctx.restore();
 
-  score.innerHTML = car.odometer.toFixed().toString();
+  score.innerHTML = bestCar.odometer.toFixed().toString();
+
+  bestCar = cars.reduce((best, next) => {
+    return best.odometer > next.odometer ? best : next;
+  }, bestCar);
 
   requestAnimationFrame(animate);
 }
